bc2d92aa2e66f2cf905adada2b8ef963f347ed11
Stéphane Laurière <slauriere@mandriva.com>
2010-05-18 09:08:24 +0000

    - added launcher and tarball maker

---
+++ tarball.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+VERSION="0.1.1"
+SVN_BASE=http://svn.mandriva.com/svn/soft/nepomuk/ginkgo
+
+rm -fr ginkgo-${VERSION}
+rm -fr ginkgo-${VERSION}.tar.bz2
+mkdir ginkgo-${VERSION}
+cd ginkgo-${VERSION}
+
+for entry in \
+ src \
+ bin \
+ po \
+ ChangeLog \
+ LICENSE \
+ README \
+ ginkgo \
+; 
+
+
+
+do svn export ${SVN_BASE}/$entry; done
+
+cd ..
+
+tar cvjf ginkgo-${VERSION}.tar.bz2 ginkgo-${VERSION}
+mv ginkgo-${VERSION}.tar.bz2 ../mdv-cooker-ginkgo/SOURCES/
+
