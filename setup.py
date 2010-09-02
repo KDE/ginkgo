6de14db7d66badb67c5858d99800528e827d6b0c
Stéphane Laurière <slauriere@mandriva.com>
2010-09-02 08:52:30 +0000

    - v0.32

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@
 
 from distutils.core import setup
 setup(name='ginkgo',
-      version='0.31',
+      version='0.32',
       url = 'http://wiki.mandriva.com/en/Ginkgo',
       download_url = 'http://svn.mandriva.com/svn/packages/cooker/ginkgo/current/SOURCES/',
       package_dir = {"": "src"},
